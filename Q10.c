a4298bf8321355161f04572dadac3639ccf4d3d7
DeivisonSantos <deivisonddj3@gmail.com>
2023-12-01 08:36:25 -0300

    Q10

--- Q10.c
+++ Q10.c
@@ -6,16 +6,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int get_Repetitions(char s_num[], char comp){
+int get_Repetitions_w_char(char s_num[], char comp){
 
     if(s_num[0] != '\0'){
         
-        if(s_num[0] == comp) return get_Repetitions(&s_num[1], comp) +1;
-        else return get_Repetitions(&s_num[1], comp);
+        if(s_num[0] == comp) return get_Repetitions_w_char(&s_num[1], comp) +1;
+        else return get_Repetitions_w_char(&s_num[1], comp);
     }
 
     return 0;
 }
+
+int get_Repetitions(int num, int comp) {
+    int qtd=0;
+
+    if(num > 0){
+
+        qtd = get_Repetitions(num/10, comp);
+
+        if(num%10 == comp) qtd++;
+    }
+
+    return qtd;
+}
+//3880
 
 int main(){
     int num, comp;
@@ -26,7 +40,12 @@ int main(){
 
     sprintf(s_num, "%d", num);
 
-    printf("Resultado: %d\n", get_Repetitions(s_num, comp+'0'));
+    if(comp < 0)
+        printf("Valor de comparacao invalido!\n");
+    else{
+        printf("Resultado 1: %d\n", get_Repetitions_w_char(s_num, comp+'0'));
+        printf("Resultado 2: %d\n", get_Repetitions(num, comp));
+    }
 
     return 0;
 }
